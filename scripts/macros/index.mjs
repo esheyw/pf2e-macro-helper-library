a0608a876ecbe5e3b655a50b4de1a9432eb57924
esheyw <esheyw@gmail.com>
2024-01-22 20:46:40 -0800

    fix macros/index.mjs

--- scripts/macros/index.mjs
+++ scripts/macros/index.mjs
@@ -2,5 +2,4 @@ export * from "./fascinatingPerformance.mjs";
 export * from "./dropHeldTorch.mjs";
 export * from "./lashingCurrents.mjs";
 export * from "./recoverOldLashingCurrents.mjs";
-export * from "./updateInitiativeSkills.mjs";
 export * from "./updateInitiativeStatistics.mjs";
